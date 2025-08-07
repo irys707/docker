88a874493356de533b213cc40138a4ce2f0601a8
irys707 <irsy707@gmail.com>
2025-08-07 08:10:57 +0000

    sadsada

--- HashCrackLabv2/entrypoint.sh
+++ HashCrackLabv2/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# Fix permissions before running the app
 
+# Create required folders if not exist
 mkdir -p /app/app/storage/uploads /app/app/storage/results
+
+# Fix ownership for appuser
 chown -R appuser:appuser /app/app/storage
 
+# Drop privileges and run Flask app
 exec gosu appuser python run.py
